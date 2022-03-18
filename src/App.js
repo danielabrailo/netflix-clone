3c5c8b0d3fc91644068957394691f4ab685792e0
Daniela Brailovsky <dbrailo@gmail.com>
2022-03-18 23:34:22 +0200

    added banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Row from './Row';
 import requests from './requests';
+import Banner from './Banner';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <Banner />
       <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
